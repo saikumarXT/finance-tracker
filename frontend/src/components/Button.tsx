a1e78af13d421c55b33123a464929571ed353d62
Sai Patel <saikumaraggu6@gmail.com>
2025-11-05 11:49:19 +0530

    react & tailwind setup done

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,7 @@
+
+
+export function Button(){
+
+    return<button className=' m-10 p-2 bg-blue-500
+     text-white border  rounded-md font-bold'>click to know more</button>
+}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+     "./index.html",
+    "./src/**/*.{jsx,ts,tsx,js}"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        blue:{
+          100:'#05DBF2',
+          200:"#04B2D9",
+          300:"#049DD9",
+          400:"#0477BF",
+          500:"#0468BF"
+        },
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
